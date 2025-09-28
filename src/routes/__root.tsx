d37bf4997a2bef38b2097f50a89c72a1675d5c42
kevohm <kevinkbet@gmail.com>
2025-09-28 23:09:21 +0300

    fix: build issue

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,21 +1,21 @@
 import { createRootRoute, Outlet } from "@tanstack/react-router";
+import { TanStackRouterDevtools } from "@tanstack/react-router-devtools";
 import React from "react";
 import ProgressBarAnimation from "../components/ProgressBarAnimation";
 import { useCurrentUser } from "../features/auth/hooks";
-import { TanStackRouterDevtools } from '@tanstack/react-router-devtools';
 
 const RootLayout = () => {
-const texts = [
-  "Track your spending in real-time",
-  "Get smart insights instantly",
-  "Manage business & personal finances",
-  "Visualize expenses with charts",
-  "Stay on top of payments & reminders",
-  "Grow your savings effortlessly",
-  "Designed for both individuals & SMEs",
-];
+  const texts = [
+    "Track your spending in real-time",
+    "Get smart insights instantly",
+    "Manage business & personal finances",
+    "Visualize expenses with charts",
+    "Stay on top of payments & reminders",
+    "Grow your savings effortlessly",
+    "Designed for both individuals & SMEs",
+  ];
 
-  const { data: user, isLoading } = useCurrentUser();
+  const { isLoading } = useCurrentUser();
 
   const [index, setIndex] = React.useState(0);
 
@@ -27,20 +27,22 @@ const texts = [
   }, []);
 
   // console.log(user);
-  if (isLoading )
+  if (isLoading)
     return (
       <div className="min-h-screen bg-white w-full flex flex-col  items-center justify-center">
-          <ProgressBarAnimation />
-          <p className="text-slate-950 text-lg font-semibold transition-opacity duration-500">
-            {texts[index]}
-          </p>
+        <ProgressBarAnimation />
+        <p className="text-slate-950 text-lg font-semibold transition-opacity duration-500">
+          {texts[index]}
+        </p>
       </div>
     );
 
-      return <>
-    <Outlet />
-    <TanStackRouterDevtools />
-  </>
+  return (
+    <>
+      <Outlet />
+      <TanStackRouterDevtools />
+    </>
+  );
 };
 
 export const Route = createRootRoute({ component: RootLayout });
