bfd31591ce0a17da00a29b8cfa057c54e7a4600a
kevohm <kevinkbet@gmail.com>
2025-09-28 22:55:09 +0300

    fix: loader

---
+++ src/components/ProgressBarAnimation.tsx
@@ -0,0 +1,18 @@
+// ProgressBarAnimation.tsx
+import React from "react";
+import Lottie from "lottie-react";
+import progressBarAnimation from "../assets/loading-animation.json";
+
+const ProgressBarAnimation: React.FC = () => {
+  return (
+    <div className="w-64 h-64">
+      <Lottie
+        animationData={progressBarAnimation}
+        loop={true} // keep looping
+        autoplay={true} // play automatically
+      />
+    </div>
+  );
+};
+
+export default ProgressBarAnimation;
--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,12 +1,13 @@
 import { createRootRoute, Outlet } from "@tanstack/react-router";
 import { TanStackRouterDevtools } from "@tanstack/react-router-devtools";
 import { useCurrentUser } from "../features/auth/hooks";
+import ProgressBarAnimation from "../components/ProgressBarAnimation";
 
 const RootLayout = () => {
   const { data: user, isLoading } = useCurrentUser();
   console.log(user)
-  if (isLoading) return <div className="h-screen flex w-full items-center justify-center">
-    <p>Loading...</p>
+  if (isLoading) return <div className="h-screen w-full flex items-center justify-center">
+    <ProgressBarAnimation/>
   </div>
 
   return <>
